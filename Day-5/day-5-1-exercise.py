86dac3d1bff78692bb2694542f95385210d5d525
NaviRocker <naveentherocker1234@gmail.com>
2022-06-23 03:28:24 +0530

    Day 5-1

---
+++ Day-5/day-5-1-exercise.py
@@ -0,0 +1,11 @@
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+total_height = 0
+for height in student_heights:
+  total_height += height
+number_of_students = 0
+for student in student_heights:
+  number_of_students += 1
+average_height = round(total_height/number_of_students)
+print(average_height)
